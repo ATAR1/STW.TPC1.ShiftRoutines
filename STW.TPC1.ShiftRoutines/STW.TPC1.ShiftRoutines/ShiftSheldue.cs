6340a33bce01ba3facfd1cd942b77791fc867a16
ATAR1 <thexenosmilu@gmail.com>
2019-01-24 16:00:09 +0500

    Коментарии

--- STW.TPC1.ShiftRoutines/STW.TPC1.ShiftRoutines/ShiftSheldue.cs
+++ STW.TPC1.ShiftRoutines/STW.TPC1.ShiftRoutines/ShiftSheldue.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace STW.TPC1.ShiftRoutines
 {
+    /// <summary>
+    /// Расписание смен при двенадцатичасовом графике
+    /// </summary>
     public class ShiftSheldue : IShiftSheldue
     {
         private readonly DateTime _startDate = new DateTime(2017, 1, 12);
@@ -12,7 +15,11 @@ namespace STW.TPC1.ShiftRoutines
         private readonly char[,] _sheldue = new char[4, 2] { { 'A', 'B' },{ 'C', 'A' },{ 'D', 'C' },{ 'B', 'D' } };
 
         private readonly TimeInterval[] _timeInterval = { new TimeInterval(8,20), new TimeInterval(20,32) };
-
+        /// <summary>
+        /// Определяет смену за дату и время
+        /// </summary>
+        /// <param name="time">Дата и время для которых нужно определить смену</param>
+        /// <returns>Смена которя является текуще по отношению ко времени time</returns>
         public Shift GetShift(DateTime time)
         {
             var difference = (time.AddHours(-8) - _startDate).TotalDays;
